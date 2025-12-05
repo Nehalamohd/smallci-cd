f3ec3393620b84f8d8d1926942c46c265610a8c4
Nehalamohd <116369351+Nehalamohd@users.noreply.github.com>
2025-12-05 12:44:34 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -3,7 +3,7 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-import pandas as pd
+
 # Load data
 X, y = load_iris(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
